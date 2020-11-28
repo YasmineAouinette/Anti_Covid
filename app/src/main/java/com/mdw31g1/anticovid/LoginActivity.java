163c50956f20d52f3892052d279911c2c90cea0f
Yasmine Aouinette <yasmineaouinette1@gmail.com>
2020-11-28 17:40:54 +0100

    Initial commit

---
+++ app/src/main/java/com/mdw31g1/anticovid/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.mdw31g1.anticovid;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView btn=findViewById(R.id.textViewSignUp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(onNewIntent(LoginActivity.this.RegisterActivity.class));
+            }
+        });
+    }
+}
