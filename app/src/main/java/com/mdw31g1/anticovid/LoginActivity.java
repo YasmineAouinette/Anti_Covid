15e5f94e12b86d5cca075f9feeb654f8b825a184
Amina <aminabkh199@gmail.com>
2020-12-29 22:16:49 +0100

    modification dans les classes java

--- app/src/main/java/com/mdw31g1/anticovid/LoginActivity.java
+++ app/src/main/java/com/mdw31g1/anticovid/LoginActivity.java
@@ -2,17 +2,53 @@ package com.mdw31g1.anticovid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-
+    //Déclaration des variables
+    TextView btn;
+    EditText inputEmail,inputPassword;
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        TextView btn=findViewById(R.id.textViewSignUp);
+
+        btn=findViewById(R.id.textViewSignUp);
+        inputEmail= findViewById(R.id.inputEmail);
+        inputPassword= findViewById(R.id.inputpassword);
+        btnLogin=findViewById(R.id.btnlogin);
+        btnLogin.setOnClickListener(v -> checkIdentifiants());
         btn.setOnClickListener(v -> startActivity (new Intent(LoginActivity.this,RegisterActivity.class)));
+    }
+    private void checkIdentifiants(){
+        String email =inputEmail.getText().toString();
+        String password=inputPassword.getText().toString();
+
+            if (email.isEmpty() || !email.contains("@")){
+                showError(inputEmail,"Email n'est pas valide!");
+            } else {
+                if (password.isEmpty() || password.length()<8){
+                    showError(inputPassword,"mot de passe n'est pas valide!");
+                }
+                else{
+                    Toast.makeText(this,"appel de la méthode de connexion!",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }
+
+
+    // affichage d'un message d'erreur au cas de saisir des données incorrect
+    private void showError(EditText input, String s) {
+        input.setError(s);
+        input.setError(s);
+        input.requestFocus();
     }
 }
--- app/src/main/java/com/mdw31g1/anticovid/RegisterActivity.java
+++ app/src/main/java/com/mdw31g1/anticovid/RegisterActivity.java
@@ -2,18 +2,65 @@ package com.mdw31g1.anticovid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class RegisterActivity extends AppCompatActivity {
+    //declaration des variables
+    TextView btn;
+    Button btnregister;
+    private FirebaseAuth mAuth;
+
+    private EditText inputUsername,inputEmail,inputPassword,inputConfirmPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        TextView btn;
-        btn = findViewById(R.id.AlreadyhaveAccount);
+
+   // Récuperation des données de view
+            btn = findViewById(R.id.AlreadyhaveAccount);
+            inputUsername = findViewById(R.id.inputPasssword);
+            inputEmail= findViewById(R.id.inputEmail);
+            inputPassword= findViewById(R.id.inputpassword);
+            inputConfirmPassword= findViewById(R.id.inputConfirmPassword);
+            btnregister=findViewById(R.id.btnregister);
+            btnregister.setOnClickListener(v -> checkIdentifiants());
+
         btn.setOnClickListener(v -> startActivity (new Intent(RegisterActivity.this,LoginActivity.class)));
+    }
+    // verification des identifiants
+    private void checkIdentifiants(){
+        String username=inputUsername.getText().toString();
+        String email =inputEmail.getText().toString();
+        String password=inputPassword.getText().toString();
+        String confirmpassword=inputConfirmPassword.getText().toString();
+       if (username.isEmpty() || username.length()<8){
+           showError(inputUsername,"votre nom n'est pas valide!");
+       }else {
+           if (email.isEmpty() || !email.contains("@")){
+               showError(inputEmail,"Email n'est pas valide!");
+           } else {
+               if (password.isEmpty() || password.length()<8){
+                   showError(inputPassword,"mot de passe n'est pas valide");
+               }else if (confirmpassword.isEmpty() || !confirmpassword.equals(password)){
+                   showError(inputConfirmPassword,"le mot de passe ne correspond pas!");
+           }
+               else{
+                   }
+
+           }
+       }
+    }
+
+ // affichage d'un message d'erreur au cas de saisir des données incorrect
+    private void showError(EditText input, String s) {
+        input.setError(s);
+        input.requestFocus();
     }
 }
