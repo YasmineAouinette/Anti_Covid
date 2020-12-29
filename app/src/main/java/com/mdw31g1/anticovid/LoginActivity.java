6304239c8e5332805a677488f832a3cb9922a2b0
Amina <aminabkh199@gmail.com>
2020-12-29 07:10:50 +0100

    correction des warnnigs

--- app/src/main/java/com/mdw31g1/anticovid/LoginActivity.java
+++ app/src/main/java/com/mdw31g1/anticovid/LoginActivity.java
@@ -1,7 +1,7 @@
 package com.mdw31g1.anticovid;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,8 +13,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         TextView btn=findViewById(R.id.textViewSignUp);
-        btn.setOnClickListener(v -> {
-            //startActivity(onNewIntent(LoginActivity.this.RegisterActivity.class));
-        });
+        btn.setOnClickListener(v -> startActivity (new Intent(LoginActivity.this,RegisterActivity.class)));
     }
 }
--- app/src/main/java/com/mdw31g1/anticovid/RegisterActivity.java
+++ app/src/main/java/com/mdw31g1/anticovid/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.mdw31g1.anticovid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,8 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        TextView btn;
+        btn = findViewById(R.id.AlreadyhaveAccount);
+        btn.setOnClickListener(v -> startActivity (new Intent(RegisterActivity.this,LoginActivity.class)));
     }
 }
